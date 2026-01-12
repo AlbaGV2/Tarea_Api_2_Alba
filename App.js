48a51b70d2a9adcd462bfbffc6d5ac5936800a9c
Alba Garcia <albagarcia@iesbalmasedabhi.com>
2026-01-12 10:14:09 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,105 @@
+//Constantes comunes para todas las partes.
+const BASE_URL = "https://rickandmortyapi.com/api";
+const estado = document.getElementById('estado');
+const result = document.getElementById('result');
+
+
+document.getElementById("limpiar").addEventListener("click", limpiar);
+
+//creo la funcion limpiar para resetear la busqueda.
+function limpiar() {
+    result.innerHTML = '';
+}
+
+
+//Parte 1 - Callback
+
+//Asignamos el evento click al boton
+document.getElementById("buscarCallback").addEventListener("click", buscarCallback);
+
+function buscarCallback() {
+
+
+//Se me complica, me parece dificil tambien de entender, los otros mas o menos los entiendo y
+//y con Algo de ayuda los voy haciendo poco a poco pero el callback se me complica.
+
+
+
+
+}
+
+
+//Parte 2 - Promise
+
+//Asignamos el evento click al boton
+document.getElementById("buscarPromesas").addEventListener("click", buscarPromesas);
+
+function buscarPromesas() {
+
+    //Recojo el input del personaje a buscar en una canstante id.
+    const id = document.getElementById('characterId').value;
+    //De mientras se muestra cargando..
+    estado.textContent = 'Cargando...';
+    //y los datos estan vacios.
+    result.innerHTML = '';
+
+    //hacemos la peticion a la Api del personaje a buscar. La API base + el input
+    fetch(`${BASE_URL}/character/${id}`)
+        //Cuando llegue la respuesta se convierte en json.
+        .then(response => response.json())
+        //Resultado final, objeto final con informacion del personaje
+        .then(data => {
+            estado.textContent = 'OK (Promesas)';
+
+            // mostramos los resultados en el html
+            result.innerHTML = `
+        <h3>${data.name}</h3>
+        <img src="${data.image}" width="150">
+        <p>Especie: ${data.species}</p>
+        <p>Estado: ${data.status}</p>
+      `;
+        }) //Error
+        .catch(error => {
+            estado.textContent = 'Error en la petición';
+        });
+
+}
+
+
+
+//Parte 3 - Async/Awayt
+
+//Evento clic para cargar con Async/Await
+document.getElementById("buscarAsync").addEventListener("click", buscarAsync);
+
+async function buscarAsync() {
+
+    // metemos en una constante el id introducido en el input
+    const id = document.getElementById('characterId').value;
+     if (!id) return; // Si no escribió nada, salimos
+
+    try {
+        // mientras se ejecuta la funcion el estado de busqueda ser este
+        estado.textContent = 'Cargando...';
+        result.innerHTML = '';
+
+        //hacemos la peticion a la Api del personaje a buscar. La API base + el input 
+        //y respuesta convertida en json.
+        const response = await fetch(`${BASE_URL}/character/${id}`);
+        const data = await response.json();
+
+        estado.textContent = 'OK!';
+        //introducimos los datos en el html
+        result.innerHTML = `
+            <h3>${data.name}</h3>
+            <img src="${data.image}" width="150">
+            <p>Especie: ${data.species}</p>
+            <p>Estado: ${data.status}</p>
+            `;
+
+        //En caso de que haya un error.
+    } catch (error) {
+        estado.textContent = 'Error en la petición';
+
+    }
+}
